1981f382b5c095c63c3876013863953b2a90b5c8
Karl Stolley <karl.stolley@gmail.com>
2021-11-22 16:45:00 -0600

    Fetch data from the GitHub API

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,15 @@ var gitHubForm = document.querySelector('#github-lookup-form');
 gitHubForm.addEventListener('submit', handleGitHubForm);
 
 function handleGitHubForm(event) {
+  var gitHubUsersApi = 'https://api.github.com/users/'
   var username = document.querySelector('#username').value;
   event.preventDefault();
   console.log("Heard submit event! Let's look up", username);
+  if ('fetch' in window) {
+    fetch(gitHubUsersApi + username).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  }
 }
