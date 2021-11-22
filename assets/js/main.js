6be29de06744dff88c0192f39a467bfc10a44ab1
Karl Stolley <karl.stolley@gmail.com>
2021-11-22 16:49:57 -0600

    Attach GitHub user data to the DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,20 @@ function handleGitHubForm(event) {
     fetch(gitHubUsersApi + username).then(function(response) {
       response.json().then(function(data) {
         console.log(data);
+        attachGitHubUser(data.name, data.avatar_url);
       });
     });
   }
+}
+
+function attachGitHubUser(username, avatar) {
+  var main = document.querySelector('main');
+  var article = document.createElement('article');
+  var h2 = document.createElement('h2');
+  var img = document.createElement('img');
+  h2.innerText = username;
+  img.src = avatar;
+  article.appendChild(h2);
+  article.appendChild(img);
+  main.appendChild(article);
 }
