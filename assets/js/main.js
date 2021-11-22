948a15709e0060cc7ba55c8a10191c3a479113db
Karl Stolley <karl.stolley@gmail.com>
2021-11-22 16:37:25 -0600

    Pull the username from the form and log it

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ var gitHubForm = document.querySelector('#github-lookup-form');
 gitHubForm.addEventListener('submit', handleGitHubForm);
 
 function handleGitHubForm(event) {
-  console.log('Heard submit event!');
+  var username = document.querySelector('#username').value;
   event.preventDefault();
+  console.log("Heard submit event! Let's look up", username);
 }
